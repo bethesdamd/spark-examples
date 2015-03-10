7b0081f9b27053734f3e19bb72cfe7825105ec19
David Swearingen <swearingendw@mac.com>
2015-03-10 17:02:33 -0400

    comments and save as text file

--- data/graph_process.scala
+++ data/graph_process.scala
@@ -7,18 +7,25 @@
 
 import org.apache.spark.graphx.Graph
 val raw = sc.textFile("50krows.txt")
+
+// Extract the two comma-delimited fields, and create a new tuple (src,dst) where src is source node,
+// dst is destination node, and the ordering is src < dst.  This ordering is necessary for some GraphX operations
 val data = raw.map { x => 
 	val s = x.split(",")
 	val a = s(0).toLong
 	val b = s(1).toLong
 	if (a > b) (b,a) else (a,b)
 }
-val graph = Graph.fromEdgeTuples(data, "v")
-val cc = graph.connectedComponents()
+val graph = Graph.fromEdgeTuples(data, "v")  // Create the graph
+val cc = graph.connectedComponents()  // Calculate the connected components
 
 // Find the triangle count for each vertex, then put the count first and the vertex id second:
 val tri = graph.triangleCount.vertices.map( x => (x._2, x._1))
+
+// Sort by triangle count.  This gives us a list of vertices with their triangle count, ordered by 
+// triangle count, so we can easily see which vertices have the largest count
 val toptris = tri.sortByKey(false)
+toptris.saveAsTextFile("/tmp/triangles")
 
 
 
