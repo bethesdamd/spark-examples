d88e0f942321ef8bd7655dc570ea64c882d480cb
David Swearingen <swearingendw@mac.com>
2015-03-10 16:51:38 -0400

    tweaks and comments

--- data/wordcount.scala
+++ data/wordcount.scala
@@ -1,19 +1,19 @@
+// Compute the frequency-of-occurence for every word in a text file,
+// and then sort ascending by frequency
+// Usage: run in spark-shell with `:load wordcount.scala`
+
 // Get the text file from filesystem
 val textFile = sc.textFile("/vagrant_data/macbeth.txt")
 
 // Split into words, map and reduce to tuples like ('thou', 12)
-val wordCounts = textFile.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey((a, b) => a + b)
+val wordCounts = textFile.flatMap(line => line.split(" ")).  // split the lines into words 
+	map(word => (word, 1)).  // put every word in a (word, 1) tuple
+	reduceByKey((a, b) => a + b)  // add up all the 1's for every word
 
 // Swap the (word, count) tuple elements so we can sort by count
 val switched = wordCounts.map( x => (x._2, x._1))
 
 // Sort and print
 switched.sortByKey(true).collect.foreach(println)
-
-// s = "979509 - - [24/Jun/1998:15:29:34 +0000] \"GET /english/competition/matchstat8862.htm HTTP/1.0\" 200 18268"
-// val pattern = """GET (.+) HTTP""".r
-// pattern.findAllIn(s).matchData.next.group(1)
-
-// wordCounts.sample(false, .01)
 
 
