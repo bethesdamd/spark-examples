3b63b383d13a627ca9df707aef1837250c2b5306
David Swearingen <swearingendw@mac.com>
2015-03-10 10:14:24 -0400

    comments

--- data/http_logs.scala
+++ data/http_logs.scala
@@ -1,14 +1,17 @@
-// Spark demo: processing world cup http logs
-// Input looks like this:
+// Spark demo: processing world cup http logs.  Input looks like this:
 // 979509 - - [24/Jun/1998:15:29:34 +0000] "GET /english/competition/matchstat8862.htm HTTP/1.0" 200 18268
+// First field is a unique client identifier that is a surrogate for an IP address
 
 import org.apache.spark.SparkContext._
 import org.apache.spark.rdd.RDD
 import scala.util.matching.Regex
 
+// Laod a text file from filesystem or HDFS and create an RDD
 val file = sc.textFile("/vagrant_data/world_cup_200k")
 
 // From the raw data RDD create a new RDD containing tuples
+// Accepts the RDD and the parsing regexp; returns RDD containing tuples like (String, 1)
+// where String is whatever the regexp extracted, e.g. a URL or IP address, etc.
 def myparse(rdd: RDD[String], regex:Regex):RDD[(String, Int)] = rdd.map { line =>
 	val md = regex.findAllIn(line).matchData
 	val s = line.split(" ")
@@ -40,8 +43,7 @@ val sorted = swap.sortByKey(false)
 // sorted is url and frequency sorted by frequency descending.  This shows the most popular url's.
 sorted.take(15).foreach(println)
 
-// Which client requested the most pages?
-val pattern = """^([\w\-]+)""".r   // Regular expression to extract url
+val pattern = """^([\w\-]+)""".r   // Regular expression to extract client_id (first 'word' in the line)
 
 // requestors = (client_id, 1)
 // (org.apache.spark.rdd.RDD[(String, Int)] ((926664,1), (1264965,1), (677046,1) ... )
@@ -51,7 +53,4 @@ val requests = myparse(file, pattern)
 val most_client_requests = requestors.reduceByKey(_ + _).  // sum all the requests, by client 
 	map(x => (x._2, x._1)).  // swap the values so we can sort by key in next step
 	sortByKey(false) // sort by number of requests, descending
-	
-	
-	
 	
